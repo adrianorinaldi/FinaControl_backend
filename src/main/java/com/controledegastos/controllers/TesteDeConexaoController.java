4684c111f9b64d155bcd42b797f57e757fb42d3a
adrianorinaldi <adriano-rinaldi@hotmail.com>
2024-04-23 20:36:52 -0300

    teste4

--- src/main/java/com/controledegastos/controllers/TesteDeConexaoController.java
+++ src/main/java/com/controledegastos/controllers/TesteDeConexaoController.java
@@ -1,20 +1,13 @@
 package com.controledegastos.controllers;
 
-import com.controledegastos.dtos.ContaDTO;
-import com.controledegastos.entities.Conta;
-import com.controledegastos.services.ContaService;
-import jakarta.validation.Valid;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
 @CrossOrigin({"*"})
 @RestController
 @RequestMapping({"/teste"})
-public class TesteDeConexãoController {
+public class TesteDeConexaoController {
 
     @GetMapping({"/teste_de_conexao"})
     public ResponseEntity<Boolean> buscarTodasContas() {
